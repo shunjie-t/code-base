2bd5f160121340097b4f5eb9245eeda7edf42b49
shunjie-t <tnshunjie@outlook.com>
2024-11-24 14:45:58 +0800

    Added third and fourth servers

---
+++ fourth/src/test/java/com/all/fourth/FourthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.all.fourth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
